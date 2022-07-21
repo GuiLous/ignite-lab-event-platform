c7224c934cbe49fd3c3ce43bbffe287d9d0a59a9
Guilherme Lourenço da Silva <guilhermesilva@acad.ifma.edu.br>
2022-07-21 08:17:39 -0300

    update node_modules

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,4 +1,4 @@
-import { Navigate, useParams } from 'react-router-dom'
+import { Navigate, useParams } from 'react-router-dom';
 
 import { Header } from '../components/Header'
 import { Sidebar } from '../components/Sidebar'
